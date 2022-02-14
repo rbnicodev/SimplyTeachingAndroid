e25402ce990a26648362c7dab01448df16d03d14
Rubén Muñoz <rubenmunoz.alu@iespacomolla.es>
2022-02-14 16:31:15 +0100

    Update Login.kt (maquetando)

--- app/src/main/java/com/rbnico/simplyteachingandroid/views/Login.kt
+++ app/src/main/java/com/rbnico/simplyteachingandroid/views/Login.kt
@@ -2,6 +2,7 @@ package com.rbnico.simplyteachingandroid.views
 
 import android.app.Application
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,16 +16,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.rbnico.simplyteachingandroid.ui.theme.SimplyTeachingAndroidTheme
+@ExperimentalCoilApi
 @Composable
 fun LoginView(
     loginOnClick : () -> Unit
 ) {
-    val onBackground = MaterialTheme.colors.onBackground
 
     Column(
         modifier = Modifier
@@ -32,10 +37,12 @@ fun LoginView(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = rememberImagePainter("https://ibb.co/MgCHGgc"),
-            contentDescription = "Foto perro",
+//            colorFilter = ColorFilter.colo,
+            painter = rememberImagePainter("https://i.ibb.co/cyr9Cy6/Logo-Aplicacion.png"),
+            contentDescription = "LogoApp",
             modifier = Modifier
-                .size(150.dp),
+                .size(150.dp)
+                .background(Color(0xFF0000)),
             contentScale = ContentScale.Crop,
         )
         Button(loginOnClick) {
